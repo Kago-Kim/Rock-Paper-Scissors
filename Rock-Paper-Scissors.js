941cdb12fd36de15b40fcf7ea598dd03f0f266d4
Kago Kimani <214457258+Kago-Kim@users.noreply.github.com>
2025-07-28 05:56:45 +0000

    ADD USER INTERFACE

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -6,18 +6,27 @@ function getComputerChoice(){
 }
 }
 
-function getHumanChoice(){
-    let a;
-while(true){
-    a=Number(prompt(`Enter 1 to choose Rock,2 to choose Paper and 3 to choose Scissors`));
+let rock=document.getElementById("rock");
+let Paper=document.getElementById("paper");
+let scissors=document.getElementById("scissors");
+let result=document.getElementById("result")
+let finalResult=document.getElementById("finalResult")
 
-    switch(a){
-        case 1:return `Rock`;
-        case 2:return  `Paper`;
-        case 3:return `Scissors`;
-        default: alert(`Enter 1 to choose Rock,2 to choose Paper and 3 to choose Scissors`)
-    }
-}}
+
+rock.addEventListener("click",function(){
+    let computerSelection = getComputerChoice();
+    playRound(`Rock`,computerSelection)
+    
+})
+paper.addEventListener("click",function(){
+    let computerSelection = getComputerChoice();
+   playRound(`Paper`,computerSelection)
+})
+scissors.addEventListener("click",function(){
+    let computerSelection = getComputerChoice();
+    playRound(`Scissors`,computerSelection) 
+})
+
 
 let humanScore=0;
 let computerScore=0;
@@ -31,35 +40,21 @@ if(
     (humanSelection===`Scissors` && computerSelection===`Paper`)
 ){
     humanScore+=1;
-    console.log(`You win!`)
+    result.textContent=`You win this round! cause you chose ${humanSelection} and i chose ${computerSelection} hence yourscore is ${humanScore} and myscore is ${computerScore}`
 }else if((humanSelection===computerSelection)){
-   console.log(`Its a tie`)
+   result.textContent=`Its a tie because we all chose ${computerSelection} and myscore is still ${computerScore} and your score is still ${humanScore}`
    tie+=1;
 }else{
      computerScore+=1;
-    console.log(`You lose!`)
+   result.textContent=`You lose this round! cause you chose ${humanSelection} and i chose ${computerSelection} hence yourscore is ${humanScore} and myscore is ${computerScore}`
 }
+if (humanScore >= 5) {
+    finalResult.textContent = `You're the Champion 🎉`;
+} else if (computerScore >= 5) {
+    finalResult.textContent = `I am the Champion 🤖`;
+}
+
 }
 
 
 
-function playGame(){
-    humanScore=0;
-    computerScore=0;
-    tie=0;
-    while((humanScore + computerScore+tie) < 5){
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        console.log(`You chose ${humanSelection}, Computer chose ${computerSelection}`);
-        playRound(humanSelection, computerSelection);
-    }
-
-    if(humanScore > computerScore){
-        console.log("Champion!");
-    } else if(humanScore==computerScore){
-        console.log("Tie");
-    }else{
-        console.log("Better luck next time");
-    }
-}
-playGame();
